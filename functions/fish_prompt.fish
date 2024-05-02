f2364f68c43679d6d4f7ac503feff4f214e74e64
max <29782397+tekoh@users.noreply.github.com>
2024-05-02 17:50:14 +0100

    works properly

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -15,20 +15,28 @@
 
 function fish_prompt
   set -l last_status $status
-  set -l cyan (set_color -o cyan)
-  set -l yellow (set_color -o yellow)
-  set -l red (set_color -o red)
-  set -l blue (set_color -o blue)
-  set -l green (set_color -o green)
+  set -l cyan (set_color cyan)
+  set -l yellow (set_color yellow)
+  set -l red (set_color red)
+  set -l blue (set_color blue)
+  set -l green (set_color green)
   set -l normal (set_color normal)
+  set -l magenta (set_color magenta)
+  set -l hostname_short (hostname -s)
+
+  set prompt_char '$'
+
+  if fish_is_root_user
+    set prompt_char '#'
+  end
 
   # adds user@hostname if on ssh connection
   if test -n "$SSH_CONNECTION"
-    set host "$cyan$USER@$blue$hostname "
+    set host "$cyan$USER$normal@$blue$hostname_short "
   end
 
   if test $last_status = 0
-      set prompt_color $green 
+      set prompt_color $normal 
   else
       set prompt_color $red
   end
@@ -43,5 +51,5 @@ function fish_prompt
   #   end
   # end
 
-  echo -n -s $host ' ' $yellow $prompt_pwd$prompt_color$normal ' '
+  echo -n -s $host $yellow (prompt_pwd) $prompt_color '$' $normal ' '
 end
